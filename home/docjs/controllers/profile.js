86397d773cacb4a91dd2376ea39509e0874232be
huaqian.song <huaqian.song@newtouch.cn>
2017-01-26 11:28:12 +0800

    home

---
+++ home/docjs/controllers/profile.js
@@ -0,0 +1,13 @@
+angular.module('app').controller('ProfileController', function($scope, $state) {
+     $scope.timeline = true;
+     $scope.select = function(option){
+         switch(option){
+         	case '1':
+         	    $scope.timeline = true;
+         	    break;
+         	case '2':
+         	    $scope.timeline = false;
+         	    break;
+         }
+     }
+});
