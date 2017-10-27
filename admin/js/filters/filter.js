3e72e0e3289a15f8c7b32a050fe044b9e2645f00
songhuaqian <766113521@qq.com>
2017-10-27 17:32:12 +0800

    接口

--- admin/js/filters/filter.js
+++ admin/js/filters/filter.js
@@ -5,7 +5,7 @@ angular.module('app')
   .filter('type', function() {
     let typeLists = [{value:"1",name:"生活类"},
     {value:"2",name:"文章类"},
-    {value:"3",name:"技术类"},
+    {value:"3",name:"书本类"},
     {value:"4",name:"译文类"}]
     return function(value){
       let newType = "";
--- article/articlejs/controllers/life.js
+++ article/articlejs/controllers/life.js
@@ -1,19 +1,54 @@
 myApp.controller('LifeCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('articlejs/controllers/life.json').then(function (resp) {
-    $scope.notes = resp.data.notes;
-    // set default note
-    $scope.note = $scope.notes[0];
-    $scope.notes[0].selected = true;
+  // $http.get('articlejs/controllers/life.json').then(function (resp) {
+  //   $scope.notes = resp.data.notes;
+  //   // set default note
+  //   $scope.note = $scope.notes[0];
+  //   $scope.notes[0].selected = true;
+  // });
+  $scope.notes = [];
+
+  //查询文章列表
+  $http.post('/api/content/list')
+  .then(function(response) {
+      if(response.status === 200){
+          $scope.notes = response.data;
+          // set default note
+          findDetail($scope.notes[0]._id);
+          $scope.notes[0].selected = true;
+      }
+  }, function(x) {
+      $scope.authError = 'Server Error';
   });
+
+  //查询文章详情
+	function findDetail(id){
+      $http.post("/api/queryContent/"+id)
+    .then(function(response) {
+        if(response.status === 200){
+          $scope.note = response.data;
+          $("#content").html(response.data.articleContent);
+        }
+    }, function(x) {
+        $scope.authError = 'Server Error';
+    });
+  }
 
   $scope.colors = ['primary', 'info', 'success', 'warning', 'danger', 'dark'];
 
-  $scope.selectNote = function(note){
-    angular.forEach($scope.notes, function(note) {
+  $scope.selectNote = function(id){
+    angular.forEach($scope.notes, function(note,index) {
       note.selected = false;
+      if(note._id === id){
+        note.selected = true;
+      }
     });
-    $scope.note = note;
-    $scope.note.selected = true;
+    findDetail(id);
+  }
+  $scope.show = true;
+  $scope.btnContent = "隐藏侧栏";
+  $scope.hideNav = function(){
+    $scope.show = $scope.show ? false:true;
+    $scope.btnContent = $scope.show?"隐藏侧栏":"打开侧栏";
   }
 
 }]);
